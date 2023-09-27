612452b10f982c35b3cdc9714e2a0fd7790bddd6
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-27 16:08:06 -0700

    added a navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <div className= 'border-8 border-red-200 font-bold'>
-        test
-      </div>
        <NavBar />
+       <Home />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,38 @@
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 const NavBar = () => {
+
+    const [nav, setNav] =useState(false)
+
+    const links = [
+        {id: '1', link: 'home'},
+        {id: '2', link: 'about'},
+        {id: '3', link: 'portfolio'},
+        {id: '4', link: 'experience'},
+        {id: '5', link: 'contact'}
+    ]
+
   return (
-    <div className= "text-4xl border-2 border-red-400 font-signature">NavBar</div>
+    <div className= "bg-black text-white flex fixed justify-between items-center w-full h-20 px-4">
+        <div>
+            <h1 className="text-5xl font-signature ml-2">David</h1>
+        </div>
+
+        <ul className='hidden md:flex'>
+            {links.map(linkObj => <li key={linkObj.id} className='cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration:200 px-4'>{linkObj.link}</li>)}
+        </ul>
+
+        <div onClick={() => setNav(!nav)}className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+            {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
+        </div>
+
+        {nav && 
+            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+                {links.map(objLink => <li key={objLink.id} className='px-4 py-6 text-4xl cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{objLink.link}</li>)}
+            </ul>
+        }
+    </div>
   )
 }
 
