c5164c9fc77790312e9df4cee8d92d0022a3ee15
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-27 15:24:17 -0700

    added tailwind and fixed issue with tailwind being applied to components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      test
+      <div className= 'border-8 border-red-200'>
+        test
+      </div>
+       <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return (
+    <div className= "text-4xl border-2 border-red-400">NavBar</div>
+  )
+}
+
+export default NavBar
