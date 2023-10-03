37b5ce34d47a79cab7441382b6a6f787dd7fb187
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-10-02 22:48:52 -0700

    updated resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
         <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
-                <p className='py-6'>Feel free to contact me. I would love to work together!</p>
+                <p className='py-6'> </p>
             </div>
 
             <div className='flex justify-center items-center'>
