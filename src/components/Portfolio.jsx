819371ec831e755c485e17e3716443fda72b7759
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-10-02 23:09:09 -0700

    changed socialLinks styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -34,13 +34,11 @@ const Portfolio = () => {
   return (
     <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen '>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-'>
+            <div className=' pb-24'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
                     Portfolio
                 </p>
-                <p className='py-6'>
-                    Check out my work!
-                </p>
+              
             </div>
 
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -8,25 +8,25 @@ const SocialLinks = () => {
     const links = [
         { 
             id: 1, 
-            child: (<> LinkedIn <FaLinkedin size={30} className="hover:bg-blue-600 rounded-sm"/> </>), 
+            child: (<> LinkedIn <FaLinkedin size={30} /> </>), 
             href: 'https://www.linkedin.com/in/brandon-zarate-estrada-b8b61313a/', 
             style: 'rounded-tr-md', 
             download: false },
         {   
             id: 2,
-            child: (<> Github <FaGithub size={30} className="hover:bg-purple-600 rounded-lg"/> </>), 
+            child: (<> Github <FaGithub size={30} /> </>), 
             href: 'https://github.com/zbrandon98', 
             style: '', 
             download: false},
         { 
             id: 3,
-            child: (<> Mail <HiOutlineMail size={30} className="hover:bg-green-500 rounded-lg"/> </>), 
+            child: (<> Mail <HiOutlineMail size={30}/> </>), 
             href: 'mailto:bdzarate@ucdavis.edu', 
             style: '', 
             download: false
         },
         {   id: 4, 
-            child: (<> Resume <BsFillPersonLinesFill size={30} className="hover:bg-yellow-500 rounded-md"/> </>), 
+            child: (<> Resume <BsFillPersonLinesFill size={30} /> </>), 
             href: '/ZarateBrandon_Resume.pdf', 
             style: 'rounded-br-md', 
             download: true 
@@ -38,7 +38,7 @@ const SocialLinks = () => {
         <ul>
 
             {links.map(({id, child, href, style, download}) => (
-                <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:rounded-md hover:ml-[10px] duration-700 bg-gray-500 ' + style}>
+                <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] border-2  border-gray-500 hover:border-red-500 hover:rounded-md hover:ml-[10px] duration-700 bg-gray-500 ' + style}>
                     <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel="noreferrer">
                         {child}
                     </a>
